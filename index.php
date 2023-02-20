af3dd2716cd7d3ca649c135f5f4daff0e797c452
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-20 16:26:12 +0100

    Fix Directory

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__ . '/models/Movies.php';
+include __DIR__ . '/models/Movie.php';
 
 $AntMan = new Movie("AntMan Quantumania", "Paul Rudd", "Scott Lang and Hope van Dyne, along with Scott's daughter Cassie Lang and Hope's parents Henry 'Hank' Pym and Janet van Dyne, accidentally find themselves trapped in the quantum realm and must face a new enemy, Kang the Conqueror.");
 $Decision = new Movie("Decision to Leave", "Tang Wei", "While investigating in the rugged Korean mountains, where the body was found, he comes across the victim's wife, Seo-rae (Tang Wei), a rather mysterious woman of Chinese origins, who has become the main suspect in the case. In fact, the woman has signs on her body that indicate a probable collision, such as scratches and bruises. What's more, she doesn't seem to show much grief over the loss of her husband.'");
